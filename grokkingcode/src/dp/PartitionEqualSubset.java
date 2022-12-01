132f14a88f12bd7ceed726f63f64701118a958a0
Nitesh Nandan <nitesh.nandan@meesho.com>
2022-12-02 01:58:16 +0530

    Equal partiton

--- grokkingcode/src/dp/PartitionEqualSubset.java
+++ grokkingcode/src/dp/PartitionEqualSubset.java
@@ -81,6 +81,53 @@ class Solution3 {
         return dp[tar];
     }
 }
+class Solution4 {
+    public static boolean canPartitionArray(int[] arr) {
+        int sum = Arrays.stream(arr).sum();
+        if (sum % 2 == 1) return false;
+        int tar = sum >> 1;
+        boolean[][] dp = new boolean[tar + 1][arr.length + 1];
+        dp[0][0] = true;
+        for (int i = 1; i <= tar; i++) dp[i][0] = false;
+        for (int i = 1; i <= arr.length; i++) dp[0][i] = false;
+
+        for(int i=1; i<=tar; i++) {
+            for(int j=1; j<=arr.length; j++) {
+                if(arr[j-1] > i) {
+                    dp[i][j] = dp[i][j-1];
+                } else {
+                    dp[i][j] = dp[i][j-1] || dp[i-arr[j-1]][j-1];
+                }
+            }
+        }
+
+        return dp[tar][arr.length];
+    }
+}
+
+// Not working
+class Solution5 {
+    public static boolean canPartitionArray(int[] arr) {
+        int sum = Arrays.stream(arr).sum();
+        if (sum % 2 == 1) return false;
+        int tar = sum >> 1;
+        boolean[][] dp = new boolean[arr.length][tar+1];
+        if(arr[0] <= tar) {
+            dp[0][arr[0]] = true;
+        }
+        for(int i=1; i<arr.length; i++) {
+            for(int k = 1; k<= tar; k++) {
+                if(arr[i] > k) {
+                    dp[i][k] = dp[i-1][k];
+                } else {
+                    dp[i][k] = dp[i-1][k] || dp[i-1][k-arr[i]];
+                }
+            }
+        }
+
+        return dp[arr.length-1][tar];
+    }
+}
 
 public class PartitionEqualSubset {
     public static void main(String[] args) {
@@ -94,6 +141,8 @@ public class PartitionEqualSubset {
         TestCaseUtil.test(true, Solution1.canPartitionArray(arr), 1);
         TestCaseUtil.test(true, Solution2.canPartitionArray(arr), 1);
         TestCaseUtil.test(true, Solution3.canPartitionArray(arr), 1);
+        TestCaseUtil.test(true, Solution4.canPartitionArray(arr), 1);
+        TestCaseUtil.test(true, Solution5.canPartitionArray(arr), 1);
     }
 
     static void test2() {
@@ -101,6 +150,8 @@ public class PartitionEqualSubset {
         TestCaseUtil.test(false, Solution1.canPartitionArray(arr), 2);
         TestCaseUtil.test(false, Solution2.canPartitionArray(arr), 2);
         TestCaseUtil.test(false, Solution3.canPartitionArray(arr), 2);
+        TestCaseUtil.test(false, Solution4.canPartitionArray(arr), 2);
+        TestCaseUtil.test(false, Solution5.canPartitionArray(arr), 2);
     }
 
     static void test3() {
@@ -108,5 +159,7 @@ public class PartitionEqualSubset {
         TestCaseUtil.test(true, Solution1.canPartitionArray(arr), 3);
         TestCaseUtil.test(true, Solution2.canPartitionArray(arr), 3);
         TestCaseUtil.test(true, Solution3.canPartitionArray(arr), 3);
+        TestCaseUtil.test(true, Solution4.canPartitionArray(arr), 3);
+        TestCaseUtil.test(true, Solution5.canPartitionArray(arr), 3);
     }
 }
