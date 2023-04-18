3ec137443a0900e4c9e524a926c51cea1dbf6082
Nitesh Nandan <nitesh.nandan@meesho.com>
2023-04-19 01:42:03 +0530

    Binary tree camera

--- leetcode/src/binarytree/BinaryTreeCameras.java
+++ leetcode/src/binarytree/BinaryTreeCameras.java
@@ -2,25 +2,26 @@ package binarytree;
 
 public class BinaryTreeCameras {
     public static int camera = 0;
+
     public static int minCameraCover(TreeNode root) {
-        if(helper(root) == -1) {
+        if (helper(root) == -1) {
             camera++;
         }
         return camera;
     }
 
     public static int helper(TreeNode root) {
-        if(root == null) return 1;
+        if (root == null) return 1;
 
         int left = helper(root.left);
         int right = helper(root.right);
 
-        if(left == -1 || right == -1) {
+        if (left == -1 || right == -1) {
             camera++;
             return 0;
         }
 
-        if(left == 0 || right == 0) {
+        if (left == 0 || right == 0) {
             return 1;
         }
         return -1;
@@ -31,7 +32,7 @@ public class BinaryTreeCameras {
     }
 
     private static void test1() {
-        TreeNode<Integer> treeNode = new TreeNode<>(0);
+        TreeNode treeNode = new TreeNode(0);
         treeNode.left = new TreeNode(0);
         treeNode.left.left = new TreeNode(0);
         treeNode.left.left.left = new TreeNode(0);
